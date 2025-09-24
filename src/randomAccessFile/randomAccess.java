fa0dc82d022b530c389a8b9ed7fbce8f5d909617
bananaboy448 <zerobananaboy@gmail.com>
2025-09-24 18:12:17 +0200

    Clase randomAccess terminada

---
+++ src/randomAccessFile/randomAccess.java
@@ -0,0 +1,68 @@
+package randomAccessFile;
+
+import java.io.*;
+import java.util.*;
+
+    public class randomAccess {
+        public static void main(String[] args) {
+            File archivo = new File("datos.bin");
+            int nums = 20;
+            ArrayList<Integer> numeros = new ArrayList<>(nums);
+            Scanner teclado = new Scanner(System.in);
+
+            if (archivo.exists()) {
+
+                try (DataInputStream dis = new DataInputStream(new FileInputStream(archivo))) {
+                    for (int i = 0; i < nums; i++) {
+                        numeros.add(dis.readInt());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(archivo))) {
+                    for (int i = 0; i < nums; i++) {
+                        dos.writeInt(0);
+                        numeros.add(0);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+
+                RandomAccessFile raf = new RandomAccessFile(archivo, "rwd");
+                int posicion;
+
+                do {
+                    System.out.println("Contenido actual:");
+                    for (int i = 0; i < nums; i++) {
+                        System.out.print(numeros.get(i) + " ");
+                    }
+                    System.out.println();
+
+                    System.out.print("Introduce la posicion para cambiar: 0-19 y negativo para salir: ");
+                    posicion = teclado.nextInt();
+
+                    if (posicion < 0) {
+                        System.out.println("Adios . . .");
+                    } else if (posicion >= 0 && posicion < nums) {
+                        System.out.print("Introduce el nuevo numero: ");
+                        int numNuevo = teclado.nextInt();
+
+                        numeros.set(posicion, numNuevo);
+
+                        raf.seek(posicion * 4L);
+                        raf.writeInt(numNuevo);
+
+                    } else {
+                        System.out.println("Posicion introducida no valida .");
+                    }
+                } while (posicion >= 0);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
